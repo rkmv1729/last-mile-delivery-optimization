595af19307134ec908a6f40302fdacf2f9182df5
rkmv1729 <shreyakmitra1729@gmail.com>
2026-08-02 03:29:49 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-sel = pd.read_parquet("data/selected_batches.parquet")
-ret = pd.read_parquet("data/retained_orders.parquet")
-print(sel.columns.tolist())
-print(ret.columns.tolist())
